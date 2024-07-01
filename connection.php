76c15e30090e6b70d41e855797649dc353214958
APRIMR <aprimregmi24@gmail.com>
2024-07-01 23:27:09 +0545

    modified about

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "fotovista";
+$server = "localhost";	//sql308.infinityfree.com
+$username = "root";//	if0_36823058
+$password = ""; //Hc5NK3AiuGBOM0
+$database = "fotovista";//if0_36823058_fotovista
 
 $conn = mysqli_connect($server , $username , $password , $database);
 
